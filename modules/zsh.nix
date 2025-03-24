705d650440e6f220d4deb2e815c9f40dc7e8a476
AceOfKestrels <aceofkestrels@gmail.com>
2025-03-24 18:06:31 +0100

    Add zsh plugins

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -1,9 +1,33 @@
-{ config, pkgs, ... }: 
+{ config, pkgs, ... }:
 
 {
-    # Enable zsh
-    programs.zsh.enable = true;
+  # Enable Zsh and set it as the default shell
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
 
-    # Set default shell
-    users.defaultUserShell = pkgs.zsh;
+  # Install Zsh plugins and Powerlevel10k
+  environment.systemPackages = with pkgs; [
+    zsh
+    zsh-powerlevel10k
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+  ];
+
+  # Configure Zsh to load the plugins
+  programs.zsh.promptInit = ''
+    [[ ! -f ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme ]] || 
+      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+
+    [[ -f /etc/zsh/p10k.zsh ]] && source /etc/zsh/p10k.zsh
+
+    [[ -f ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]] && 
+      source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+    [[ -f ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]] && 
+      source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+    ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=8'
+
+    ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
+  '';
 }
