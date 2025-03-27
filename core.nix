599410c82f550234bd8a6f10629b80e4f3d14a0b
Annika <mail@annika-keggenhoff.de>
2025-03-27 23:23:46 +0100

    added shell-source.nix that clones or pulls the shell source repo

--- core.nix
+++ core.nix
@@ -7,6 +7,7 @@
         ./modules/gnome.nix # GNOME
         ./modules/cli/zsh.nix # ZSH
         ./modules/vscode.nix # VS Code / VSCodium
+	./modules/cli/shell-sources.nix # Loads the Shell-Source
     ];
 
     # Include core packages
